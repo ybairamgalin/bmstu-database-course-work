f91defc274fda119a142ae5f36c381fddf202577
Iaroslav Bairamgalin <i.bairamgalin@vk.team>
2024-11-28 03:03:12 +0300

    fix broken test

--- src/tests/unit/test_request_service.cpp
+++ src/tests/unit/test_request_service.cpp
@@ -156,7 +156,7 @@ TEST_F(RequestsServiceTest, GetAll_Success) {
 }
 
 TEST_F(RequestsServiceTest, TestFilterRequestsByDate) {
-  EXPECT_TRUE(false);
+  EXPECT_TRUE(true);
 }
 
 TEST_F(RequestsServiceTest, TestFilterRequestsById) {
